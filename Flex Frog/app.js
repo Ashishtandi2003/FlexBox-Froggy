ea778c3f28c2e5be05751c75920ae5723c40877f
Ashish Tandi <141714696+Ashishtandi2003@users.noreply.github.com>
2024-07-28 21:06:04 +0530

    Add files via upload

---
+++ Flex Frog/app.js
@@ -0,0 +1,18 @@
+const buttonElement = document.querySelector('button')
+const textAreaElement = document.querySelector('textarea')
+const foreAtt = document.querySelector('.forefront')
+
+function addCSS() {
+   const css = textAreaElement.value
+   foreAtt.style.cssText = css
+}
+
+function nextLevel(){
+   let btnInput = document.querySelector('.forefront');
+   window.location.href = "main2.html";  
+  
+}
+
+textAreaElement.addEventListener('keydown',addCSS)
+
+buttonElement.addEventListener('click', nextLevel)
